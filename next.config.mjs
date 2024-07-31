8c8328c134e74d67c929b98bf1959b3426e5a0c5
praveen kumar <praveenhtml5developer@gmail.com>
2024-07-31 19:24:25 +0800

    fixing mjs issue

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+// import createNextIntlPlugin from 'next-intl/plugin';
+ 
+// const withNextIntl = new createNextIntlPlugin();
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+ 
+export default nextConfig;
