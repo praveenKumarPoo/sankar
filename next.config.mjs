cda534b7ded16319fe70bbfaeeb3fa9404060232
praveen kumar <praveenhtml5developer@gmail.com>
2024-07-31 21:21:58 +0800

    fixing build issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,8 @@
-import nextIntl from "next-intl/plugin";
-
-// Your existing Next.js configuration
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: ["tailwindui.com"],
-  },
-};
-
-// Use nextIntl to extend your configuration with the next-intl plugin
-const withNextIntl = nextIntl("./i18n.ts");
-
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+const withNextIntl = createNextIntlPlugin();
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+ 
 export default withNextIntl(nextConfig);
